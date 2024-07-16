63db309645e3dd3f5aec5ee477cf8b0c6a2c4a4a
jesse-rr <jessericardorogerio@gmail.com>
2024-07-16 19:18:19 -0300

    Tracking + Fire implementado 16/07

--- jesserr/Paranoia.java
+++ jesserr/Paranoia.java
@@ -1,57 +1,50 @@
 package jesserr;
 import robocode.*;
-//import java.awt.Color;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+public class Paranoia extends AdvancedRobot {// tem q set AdvancedRobot
 
-/**
- * Paranoia - a robot by (your name here)
- */
-public class Paranoia extends Robot
-{
-	/**
-	 * run: Paranoia's default behavior
-	 */
 	public void run() {
-		// Initialization of the robot should be put here
-		turnRadarRight(Double.POSITIVE_INFINITY);
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
+		turnRadarLeft(Double.POSITIVE_INFINITY);
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+		// nao precisa, "acho"
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		
+		double frente = e.getBearingRadians() + getHeadingRadians(); // achar qual direcao o tank vai virrar com enem. frente + minha frente;
+		double gunAng; // angulo da arma, definir dps
+		
+		setTurnRadarLeftRadians(getRadarTurnRemainingRadians());
+		gunAng = robocode.util.Utils.normalRelativeAngle(frente - getGunHeadingRadians()); // definir angulo da arma (variavel) -> movimento em repetiçao, mas n pode colocar no while loop, ent bota aqui msm
+		setAhead((e.getDistance() - 120)); // para aproximar inimigo -> movimento em repetiçao, mas n pode colocar no while loop, ent bota aqui msm
+		
+		// pegar distancia inimiga, pra variar força de tiro
+		if (e.getDistance() > 130) { 
+		
+			setTurnGunRightRadians(gunAng); // setar o ang da arma, tem q ser dps do primeiro
+			setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(frente - getHeadingRadians()));
+			
+			fire(2); // fire mais fraco pq mais distancia = mais facil de desviar
+		}
+		else { 
+			
+			setTurnGunRightRadians(gunAng); 
+			setTurnRight(90 - e.getBearing());
+			
+			fire(3); // fire mais forte pq menos distancia = mais dificil de desviar
+		}
 	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+
+		// nao precisa, "acho"
+	}
+	
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
+
+		// nao precisa, "acho"
 	}	
 }
